5ab2887c00727d347d37c8b7df60a17c2ed50f07
backendbro <ukagha.nzubechukwu@yahoo.com>
2023-11-02 11:41:54 -0400

    Learnt about Declarative and Imperative programming

--- Exercise1/index.js
+++ Exercise1/index.js
@@ -1,9 +1,34 @@
-ReactDOM.render(
+// DECLARATIVE
 
-<ul>
-    <li>It's super easy</li>
-    <li>I love easy stuffs, lol</li>
-</ul>
+// function Navbar () {
+//     return (
+//         <div className="navbar">
+//             <a href="#home">Home</a>
+//             <a href="#about">About</a>
+//             <a href="#services">Services</a>
+//             <a href="#portfolio">Portfolio</a>
+//             <a href="#contact">Contact</a>
+//         </div>
+//     )
 
+// }
 
-, document.getElementById("root"))
+// ReactDOM.render(
+
+// <div>
+//     <Navbar />
+// </div>
+
+// , document.getElementById("root"))  
+
+// IMPERATIVE 
+
+// function displayContent () {
+//     const headerOne = document.createElement("h1")
+//     headerOne.textContent = "Hello, I am a header!"
+//     headerOne.className = "header"
+
+//     document.getElementById('root').appendChild(headerOne)
+// }
+
+// displayContent()
