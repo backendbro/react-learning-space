634b09b1894585bb81a3b3e3974876457d502092
backendbro <ukagha.nzubechukwu@yahoo.com>
2023-11-04 18:46:01 -0400

    Done with my first project

---
+++ exercise4/src/App.js
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+
+function App() {
+  const content = (
+    <div className="container">
+      <Navbar /> 
+      <Main /> 
+    </div>
+  )
+
+  return content
+}
+
+export default App;
---
+++ exercise4/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+function Navbar () {
+    const content = (
+        <nav>
+             <div className="left-section">
+                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ficonape.com%2Fwp-content%2Fpng_logo_vector%2Freact-2.png&f=1&nofb=1&ipt=b92ffe8f1b646edb6cea7929d112ce25ffc2575974f3a4b04c5f739f38d7ddef&ipo=images"/>
+                <h3>React Facts</h3>
+             </div>
+             
+             <div className="right-section">
+                <h4>React course - Project 1</h4>
+             </div>
+        </nav>
+    )
+
+    return content
+}
+
+export default Navbar 
